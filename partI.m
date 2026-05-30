d37da32bfcf1ad2236d802093fac35a840cb72ae
kurtc <kurtc@k>
2026-05-30 15:52:08 +1000

    construct linear dti system and allocate storages

--- partI.m
+++ partI.m
@@ -2,3 +2,13 @@ clear
 load partI.mat
 whos
 
+% Construct linear DTI system
+A1 = g .* g;
+A2 = 2 * [g(:,1).*g(:,2), g(:,1).*g(:,3), g(:,2).*g(:,3)];
+A = [A1, A2];   % 64 x 6
+
+% Allocate storage
+[nx, ny, ndirs] = size(S);
+
+D_field  = zeros(nx, ny, 6);
+D_tensor = zeros(nx, ny, 3, 3);
