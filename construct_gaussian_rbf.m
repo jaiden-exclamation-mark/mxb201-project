7c072b20dc99c825f17a458ba5850f4690e5d80f
jaiden-exclamation-mark <jaidenthomas271@outlook.com>
2026-05-29 23:36:59 -0700

    Alter `construct_gaussian_rbf() to return data and prediction matrices

--- construct_gaussian_rbf.m
+++ construct_gaussian_rbf.m
@@ -1,15 +1,36 @@
-function phi = construct_gaussian_rbf(points, centres, epsilon)
+function [data, prediction] = construct_gaussian_rbf(points, epsilon, rho, diffusion_tensors)
     num_voxels = length(points);
-    num_centres = length(centres);
-    phi = zeros(num_voxels, num_centres);
+    data = zeros(num_voxels, 6);
 
-    for index = 1:num_centres
+    for index = 1:num_voxels
 
-        dx = points(:,1) - centres(index, 1);
-        dy = points(:,2) - centres(index, 2);
+        i = points(index, 1);
+        j = points(index, 2);
+
+        D_b = squeeze(diffusion_tensors(i, j, :, :));
+        D_b = make_spd(D_b);
+
+        A_b = logm(D_b);
+        A_b = real(0.5 * (A_b + A_b.'));
+
+        data(index, :) = [A_b(1,1), A_b(2,2), A_b(3,3), ...
+                          A_b(1,2), A_b(1,3), A_b(2,3)];
+    end
+
+    phi = zeros(num_voxels);
+
+    for index = 1:num_voxels
+        dx = points(:, 1) - points(index, 1);
+        dy = points(:, 2) - points(index, 2);
 
         r_squared = dx.^2 + dy.^2;
 
         phi(:, index) = exp(-(epsilon^2) * r_squared);
     end
+    
+    % Solve regularized least squares
+    M = phi.' * phi + rho * eye(num_voxels);
+
+    coefficients = M \ (phi.' * data);
+    prediction = phi * coefficients;
 end
